f511ee3c6b8f55809832f999b7b834161580eef5
hiroki nagao <honghuizhangwei@gmail.com>
2021-07-23 15:25:04 +0900

    ログイン機能

--- util.php
+++ util.php
@@ -53,6 +53,56 @@ function convertToDayTimeAgo(string $datetime)
  
     return (int)$time . $unit;
 }
- 
- 
- 
+ function saveUserSession(array $user)
+ {
+     //セッションを開始していない場合
+     if(session_status() === PHP_SESSION_NONE){
+         //セッション開始
+         session_start();
+         
+     }
+     $_SESSION['USER'] = $user;
+ }
+
+ /**
+  *ユーザー情報をセッションから削除
+  *@return void 
+  */
+  function deleteUserSession()
+  {
+       //セッションを開始していない場合
+     if(session_status() === PHP_SESSION_NONE){
+        //セッション開始
+        session_start();
+        
+  }
+  //セッションユーザーの情報を削除
+  unset($_SESSION['USER']);
+}
+
+/**
+ * @return array|false
+ * 
+ * 
+ */
+function getUserSession()
+{
+    //セッションを開始していない場合
+    if(session_status() === PHP_SESSION_NONE){
+        //セッション開始
+        session_start();
+
+}
+if(!isset($_SESSION['USER'])){
+    //セッションにユーザー情報がない
+    return false;
+}
+$user = $_SESSION['USER'];
+
+//画像のファイル名からファイルのurlを習得
+if(!isset($user['image_name'])){
+    $user['image_name'] = null;
+}
+$user['image_path'] = buildImagePath($user['image_name'],'user');
+return $user;
+}
