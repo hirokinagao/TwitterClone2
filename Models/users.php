2c627a31a091d420b98c37e19a8eead2ffeb67ab
hiroki nagao <honghuizhangwei@gmail.com>
2021-08-17 20:04:38 +0900

    追加

--- Models/users.php
+++ Models/users.php
@@ -2,7 +2,6 @@
 ///////////////////////////////////////
 // ユーザーデータを処理
 ///////////////////////////////////////
- 
 /**
  * ユーザーを作成
  *
@@ -17,31 +16,24 @@ function createUser(array $data)
         echo 'MySQLの接続に失敗しました。：' . $mysqli->connect_error . "\n";
         exit;
     }
- 
     // 新規登録のSQLを作成
     $query = 'INSERT INTO users (email, name, nickname, password) VALUES (?, ?, ?, ?)';
     $statement = $mysqli->prepare($query);
- 
     // パスワードをハッシュ値に変換
     $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
- 
     // ? の部分にセットする内容
     // 第一引数のsは変数の型を指定(s=string)
     $statement->bind_param('ssss', $data['email'], $data['name'], $data['nickname'], $data['password']);
- 
     // 処理を実行
     $response = $statement->execute();
     if ($response === false) {
         echo 'エラーメッセージ：' . $mysqli->error . "\n";
     }
- 
     // 接続を解放
     $statement->close();
     $mysqli->close();
- 
     return $response;
 }
- 
 /**
  * ユーザーを更新
  *
@@ -56,15 +48,12 @@ function updateUser(array $data)
         echo 'MySQLの接続に失敗しました。：' . $mysqli->connect_error . "\n";
         exit;
     }
- 
     // 更新日時
     $data['updated_at'] = date('Y-m-d H:i:s');
- 
     if (isset($data['password'])) {
         // パスワードをハッシュ値に変換
         $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
     }
- 
     // 更新のSQLを作成
     // SET句のカラムを準備
     $set_columns = [];
@@ -76,23 +65,17 @@ function updateUser(array $data)
             $set_columns[] = $column . ' = "' . $mysqli->real_escape_string($data[$column]) . '"';
         }
     }
- 
     $query = 'UPDATE users SET ' . join(',', $set_columns);
     $query .= ' WHERE id = "' . $mysqli->real_escape_string($data['id']) . '"';
- 
     // SQLを実行
     $response = $mysqli->query($query);
- 
     // 結果が失敗の場合、エラー表示
     if ($response === false) {
         echo 'エラーメッセージ：' . $mysqli->error . "\n";
     }
- 
     $mysqli->close();
- 
     return $response;
 }
- 
 /**
  * ユーザー情報取得：ログインチェック
  *
@@ -108,14 +91,11 @@ function findUserAndCheckPassword(string $email, string $password)
         echo 'MySQLの接続に失敗しました。：' . $mysqli->connect_error . "\n";
         exit;
     }
- 
     // 入力値をエスケープ
     $email = $mysqli->real_escape_string($email);
- 
     // クエリを作成
     // - 外部からのリクエストは何が入ってくるかわからないので、必ず、エスケープしたものをクオートで囲む
     $query = 'SELECT * FROM users WHERE email = "' . $email . '"';
- 
     // SQL実行
     $result = $mysqli->query($query);
     if (!$result) {
@@ -124,7 +104,6 @@ function findUserAndCheckPassword(string $email, string $password)
         $mysqli->close();
         return false;
     }
- 
     // ユーザー情報を取得
     $user = $result->fetch_array(MYSQLI_ASSOC);
     if (!$user) {
@@ -132,27 +111,25 @@ function findUserAndCheckPassword(string $email, string $password)
         $mysqli->close();
         return false;
     }
- 
     // パスワードチェック
     if (!password_verify($password, $user['password'])) {
         // パスワード不一致
         $mysqli->close();
         return false;
     }
- 
     $mysqli->close();
- 
     return $user;
 }
- 
 /**
  * ユーザーを取得
  *
  * @param integer $user_id
  * @param integer $login_user_id
+ * @param string $email 未実装
+ * @param string $name 未実装
  * @return array|false
  */
-function findUser(int $user_id, int $login_user_id = null)
+function findUser(int $user_id = null, int $login_user_id = null, string $email = null, string $name = null)
 {
     $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     // 接続チェック
@@ -160,11 +137,9 @@ function findUser(int $user_id, int $login_user_id = null)
         echo 'MySQLの接続に失敗しました。：' . $mysqli->connect_error . "\n";
         exit;
     }
- 
     // エスケープ
     $user_id = $mysqli->real_escape_string($user_id);
     $login_user_id = $mysqli->real_escape_string($login_user_id);
- 
     // 検索のSQLを作成
     $query = <<<SQL
         SELECT
@@ -187,7 +162,6 @@ function findUser(int $user_id, int $login_user_id = null)
         WHERE
             U.status = 'active' AND U.id = '$user_id'
     SQL;
- 
     // SQLを実行
     if ($result = $mysqli->query($query)) {
         // データを配列で返却
@@ -197,8 +171,6 @@ function findUser(int $user_id, int $login_user_id = null)
         $response = false;
         echo 'エラーメッセージ：' . $mysqli->error . "\n";
     }
- 
     $mysqli->close();
- 
     return $response;
 }
